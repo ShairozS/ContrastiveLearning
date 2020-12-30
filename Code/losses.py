639ada28f443d2466ad79c322b4bee972bed2bec
Shairoz Sohail <ShairozSohail@gmail.com>
2020-12-29 21:51:56 -0600

    Update losses.py

--- Code/losses.py
+++ Code/losses.py
@@ -1,26 +1,46 @@
 from torch import nn
 
 
-class TemplateLoss(nn.Module):
+class ContrastiveLoss(torch.nn.Module):
+    
+    
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    
+    args:
+        distance (function): A function that returns the distance between two tensors - should be a valid metric over R; default=nn.PairwiseDistance()
+        margin (scalar): The margin value between positive and negative class ; default=1.0
+        pos_class_weight (scalar): The weight to put on positive class loss when positive classes are sparse ; default=20
+    """
+
+    
+    def __init__(self,
+                 distance = nn.PairwiseDistance(),
+                 margin=1.0,
+                 pos_class_weight=20,
+                 reduction='mean'):
+        
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+        self.distance = distance
+        self.pos_class_weight = pos_class_weight
+        self.reduction = reduction
+
+        
+        
+    def forward(self, output1, output2, label):
+        
+        euclidean_distance = self.distance(output1, output2)
+        
+        # When the label is 1 (similar) - the loss is the distance between the embeddings
+        # When the label is 0 (dissimilar) - the loss is the distance between the embeddings and a margin
+        if self.reduction == 'mean':
+            loss_contrastive = torch.mean((label) * torch.pow(euclidean_distance, 2) +
+                                      (self.pos_class_weight*(1-label)) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        if self.reduction == 'sum':
+            loss_contrastive = torch.sum((label) * torch.pow(euclidean_distance, 2) +
+                                      (self.pos_class_weight*(1-label)) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
 
 
-      def __init__(self, hyper_params):
-
-            '''
-            Initialize the loss function here with any specific hyperparameters
-            '''
-            pass
-
-
-
-      def forward(self, y, yhat):
-
-            '''
-            Return the scalar loss given two inputs y and yhat
-            '''
-
-            ## Some processing here
-            loss = torch.sqrt(torch.sum((y - yhat)**2))
-
-
-            return(loss)
+        return loss_contrastive
